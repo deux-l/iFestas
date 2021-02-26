2a2b6648b3a492a0171a1330318e5addcf20f275
ll <deuxl@outlook.com>
2021-02-26 11:44:54 -0300

    update visualPages

--- frontend/src/app/termos/termos.component.ts
+++ frontend/src/app/termos/termos.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TermosComponent implements OnInit {
 
+  dataAtual: Date = new Date()
+
   constructor() { }
 
   ngOnInit(): void {
